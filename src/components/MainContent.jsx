def9f561eefc7eb9cb551ecbe4c780792f833f5a
Zain Ali <zain8511@gmail.com>
2022-03-30 17:47:47 +0500

    1st commit, Nav& hero added

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../css/Content.module.css'
+import contentImage from '../assets/charImg1.png'
+
+
+
+export const MainContent = () => {
+    return (
+        <div className={styles.contentMain }>
+            
+        </div>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from '../css/Navbar.module.css'
+
+function Navbar() {
+    return (
+        <div className={styles.navbar}>
+            <div className={styles.content }>
+                <a href="" className={ styles.active }>Home</a>
+                <a href="">Utility</a>
+                <a href="">About Us</a>
+                <a href="">JOIN THE MOVEMENT</a>
+
+                <div>
+                    <img src="https://static.wixstatic.com/media/3ee487_24c46d2335e74f07aeabf0ee35da789d~mv2.png/v1/fill/w_38,h_38,al_c,usm_0.66_1.00_0.01,enc_auto/3ee487_24c46d2335e74f07aeabf0ee35da789d~mv2.png" alt="Twitter Icon" />
+                </div>
+                <div>
+                    <img src="https://static.wixstatic.com/media/3ee487_e5ed8539c57a4ddcb393b0cdf34ab359~mv2.png/v1/fill/w_38,h_38,al_c,usm_0.66_1.00_0.01,enc_auto/3ee487_e5ed8539c57a4ddcb393b0cdf34ab359~mv2.png" alt=" Discord" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
